2712382f8450b618c7697ae20296d71e68cc6f1a
Bjarke Hesthaven Søndergaard <bhsgaard@gmail.com>
2018-08-07 11:15:04 +0200

    The aliased action needs to be imported

--- lib/fastlane/plugin/tpa/actions/tpa_action.rb
+++ lib/fastlane/plugin/tpa/actions/tpa_action.rb
@@ -1,5 +1,6 @@
 module Fastlane
   module Actions
+    require_relative 'upload_to_tpa_action'
     class TpaAction < UploadToTpaAction
       def self.description
         "Alias for the `upload_to_tpa` action"
