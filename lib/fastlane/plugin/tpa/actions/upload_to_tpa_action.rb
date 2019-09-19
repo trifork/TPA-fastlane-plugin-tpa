6587fd8a9adccc08b184d2a0206d9b0b795901d9
Mads Bøgeskov <mbg@danskebank.dk>
2019-09-19 08:29:04 +0200

    Response from upload is parsed

--- lib/fastlane/plugin/tpa/actions/upload_to_tpa_action.rb
+++ lib/fastlane/plugin/tpa/actions/upload_to_tpa_action.rb
@@ -1,7 +1,9 @@
 module Fastlane
   module Actions
     module SharedValues
+      # Download URL
       TPA_INSTALL_URL = :TPA_INSTALL_URL
+      # URL to TPA.io where you can see this build
       TPA_BUILD_URL = :TPA_BUILD_URL
     end
 
